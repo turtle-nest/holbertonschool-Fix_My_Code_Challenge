72901c96bfd1ce25fc9c370e7706f9ab45914700
turtle-nest <n78693107@gmail.com>
2025-05-28 11:11:49 +0200

    4. Add task

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -2,52 +2,46 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Delete a node at a specific index from a list
- *
- * @head: A pointer to the first element of a list
- * @index: The index of the node to delete
+ * delete_dnodeint_at_index - deletes the node at index of a dlistint_t list
+ * @head: double pointer to head of list
+ * @index: index of node to delete
  *
  * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *saved_head;
-	dlistint_t *tmp;
-	unsigned int p;
+	dlistint_t *node = *head;
+	unsigned int i = 0;
 
-	if (*head == NULL)
-	{
+	if (!head || !*head)
 		return (-1);
-	}
-	saved_head = *head;
-	p = 0;
-	while (p < index && *head != NULL)
+
+	/* Delete head node */
+	if (index == 0)
 	{
-		*head = (*head)->next;
-		p++;
+		*head = node->next;
+		if (*head)
+			(*head)->prev = NULL;
+		free(node);
+		return (1);
 	}
-	if (p != index)
+
+	/* Traverse to node at index */
+	while (node && i < index)
 	{
-		*head = saved_head;
+		node = node->next;
+		i++;
+	}
+
+	if (!node)
 		return (-1);
-	}
-	if (0 == index)
-	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
-		if (tmp != NULL)
-		{
-			tmp->prev = NULL;
-		}
-	}
-	else
-	{
-		(*head)->prev->prev = (*head)->prev;
-		free(*head);
-		if ((*head)->next)
-			(*head)->next->prev = (*head)->prev;
-		*head = saved_head;
-	}
+
+	/* Unlink the node */
+	if (node->prev)
+		node->prev->next = node->next;
+	if (node->next)
+		node->next->prev = node->prev;
+
+	free(node);
 	return (1);
 }
